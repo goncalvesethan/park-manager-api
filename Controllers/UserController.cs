dc781e4af5c98f6d427aca63712fb4098a4d9c29
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-02-21 11:52:28 +0100

    feat: Create UserController to return at API users endpoint
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

---
+++ Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParkManagerAPI.Models;
+
+namespace ParkManagerAPI.Controllers;
+
+[ApiController]
+[Route("api/users")]
+
+public class UserController : ControllerBase
+{
+    private readonly ParkManagerContext _context;
+
+    public UserController(ParkManagerContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<User>>> GetAll()
+    {
+        var users = await _context.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetById(int id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null) return NotFound("User not found !");
+        
+        return Ok(user);
+    }
+}
