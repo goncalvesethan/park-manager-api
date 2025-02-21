8db4cf071c2b4ffa1eb8abbb95228dcce26b41b9
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-02-21 11:51:54 +0100

    feat: Create User model
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

---
+++ Models/User.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParkManagerAPI.Models;
+
+/// <summary>
+/// Represents a user
+/// </summary>
+public class User
+{
+    /// <summary>
+    /// User ID (primary key)
+    /// </summary>
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+    
+    /// <summary>
+    /// Lastname (required)
+    /// </summary>
+    [Required]
+    [MaxLength(50)]
+    public required string Lastname { get; set; }
+    
+    /// <summary>
+    /// Firstname (required)
+    /// </summary>
+    [Required]
+    [MaxLength(50)]
+    public required string Firstname { get; set; }
+    
+    /// <summary>
+    /// Email (required)
+    /// </summary>
+    [Required]
+    [EmailAddress]
+    [MaxLength(100)]
+    public required string Email { get; set; }
+    
+    
+    /// <summary>
+    /// Password (required)
+    /// </summary>
+    [Required]
+    [DataType(DataType.Password)]
+    public required string Password { get; set; }
+    
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime? DisabledAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
+}
