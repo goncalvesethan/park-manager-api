5a2f654b19d7d2911f115773915585bf75983963
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-03-10 16:37:09 +0100

    feat: Init authentication with JWT
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ParkManagerAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +17,25 @@ builder.Services.AddSwaggerGen();
 // Add the database context
 builder.Services.AddDbContext<ParkManagerContext>(options => options.UseSqlite(builder.Configuration.GetConnectionString("ParkManagerDb")));
 
+// JWT configuration
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+    options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<JwtService>();
 
 var app = builder.Build();
 
@@ -22,6 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
