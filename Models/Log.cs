ed6015e359182227eb18744d309502b9eadbb6bc
Ethan GONÇALVES <goncalves.ethan@gmail.com>
2025-04-09 21:25:58 +0200

    feat: Create Log model with attributes, add table and implement DbSet in context
    Signed-off-by: Ethan GONÇALVES <goncalves.ethan@gmail.com>

---
+++ Models/Log.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParkManagerAPI.Models;
+
+/// <summary>
+/// Représente un log.
+/// </summary>
+public class Log
+{
+    /// <summary>
+    /// ID du log (clé primaire).
+    /// </summary>
+    [Key]
+    [Column("id")]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Type de log (info, warning, error).
+    /// </summary>
+    [Required]
+    [MaxLength(50)]
+    public required string Type { get; set; }
+
+    /// <summary>
+    /// Ressource concernée.
+    /// </summary>
+    [Required]
+    [MaxLength(50)]
+    public required string Resource { get; set; }
+
+    /// <summary>
+    /// Méthode associée au log.
+    /// </summary>
+    [Required]
+    [MaxLength(100)]
+    public required string Method { get; set; }
+
+    /// <summary>
+    /// Message du log
+    /// </summary>
+    [Required]
+    public required string Message { get; set; }
+
+    /// <summary>
+    /// Date et heure de création du log.
+    /// </summary>
+    public DateTime CreatedAt { get; set; }
+
+    /// <summary>
+    /// Date et heure de la dernière mise à jour du log.
+    /// </summary>
+    public DateTime UpdatedAt { get; set; }
+    
+    /// <summary>
+    /// Date et heure de la suppression du log.
+    /// </summary>
+    public DateTime? DeletedAt { get; set; }
+}
